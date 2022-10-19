e22950f218a18a5559440142aa38d1118d15229d
Baransel DİNÇ <baranseldinc@gmail.com>
2022-10-20 02:53:19 +0300

    firebase added

---
+++ public/persistence.js
@@ -0,0 +1,66 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
+import { getDatabase, ref, child, get, set } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-database.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB6xgxqhBHdLcJXfPUus5KZotLSPl3Yd2U",
+    authDomain: "perfectionist-english-app.firebaseapp.com",
+    databaseURL: "https://perfectionist-english-app-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "perfectionist-english-app",
+    storageBucket: "perfectionist-english-app.appspot.com",
+    messagingSenderId: "834998132835",
+    appId: "1:834998132835:web:cd08dafef44507504e1856",
+    measurementId: "G-55WW6NPPQ8"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const dbRef = ref(database);
+window.persistence = {};
+
+window.persistence.getAllData = function (successCallback, errorCallback) {
+    get(child(dbRef, `availablities`)).then((snapshot) => {
+        if (snapshot.exists()) {
+            successCallback(snapshot.val());
+        }
+    }).catch((error) => {
+        errorCallback(error)
+    });
+}
+
+window.persistence.getUserData = function (username, successCallback, errorCallback) {
+    get(child(dbRef, `availablities/${username}`)).then((snapshot) => {
+        if (snapshot.exists()) {
+            successCallback(snapshot.val());
+        }
+    }).catch((error) => {
+        errorCallback(error)
+    });
+}
+
+window.persistence.checkUserName = function (username, callback) {
+    get(child(dbRef, `availablities/${username}`)).then((snapshot) => {
+        if (snapshot.exists()) {
+            callback(false);
+        } else {
+            callback(true);
+        }
+
+    }).catch((error) => {
+        callback(true, error)
+    });
+}
+
+window.persistence.insertResponse = function (username, response, successCallback, errorCallback) {
+    localStorage.setItem('username', username);
+    set(ref(database, 'availablities/' + username), {
+        response: response,
+        total_update: 0,
+        last_update: new Date().toISOString()
+    }).then(() => {
+        successCallback();
+    }).catch(e => {
+        errorCallback(e);
+    });
+}
--- public/survey.js
+++ public/survey.js
@@ -1,4 +1,4 @@
-const selectedHours = [];
+var selectedHours = [];
 const allHours = document.querySelectorAll('.hour-box');
 const shortDays = {
     pazartesi: 'Pzt',
@@ -34,3 +34,58 @@ Array.from(allHours).forEach(hour => {
         console.log(selectedHours);
     });
 })
+const username = localStorage.getItem('username');
+
+setTimeout(() => {
+    if (username) {
+        window.persistence.getUserData(username, data => {
+            window.userData = data;
+            selectedHours = data.response;
+            renderTable();
+        }, error => {
+            alert(username + ' kullanıcısına ait veriler sunucudan alınamadı.');
+            console.log(error);
+        })
+    }
+}, 2000);
+
+
+function renderTable() {
+    selectedHours.forEach(item => {
+        const dataArr = item.split('#');
+        const parentValue = dataArr[0];
+        const value = dataArr[1];
+
+        const element = document.querySelector(`.day-box[data-value="${parentValue}"] .hour-box[data-value="${value}"]`)
+        element.classList.toggle('bg-selected');
+    })
+}
+
+function kaydet() {
+    if (username) {
+        window.persistence.updateUserResponse(username, selectedHours, () => {
+            alert('Başarıyla güncellendi..!');
+        }, err => {
+            alert('HATA: güncelleme işlemi başarısız!');
+            console.log(err);
+        });
+    } else {
+        let xusername = prompt('Kendiniz için bir kullanıcı adı belirtin:');
+        if (!xusername) {
+            return;
+        }
+        window.persistence.checkUserName(xusername, success => {
+            if (success) {
+                window.persistence.insertResponse(xusername, selectedHours, () => {
+                    alert('Cevaplarınız başarıyla kaydedildi');
+                }, () => {
+                    alert('HATA: kaydetme işlemi başarısız');
+                    console.log(err);
+                });
+            } else {
+                alert(`'${xusername}' başka bir kullanıcı tarafından kullanılıyor. Lütfen başka bir kullanıcı adı deneyin.`);
+                return;
+            }
+        })
+    }
+}
